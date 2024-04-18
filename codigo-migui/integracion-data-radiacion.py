a0665b6e578d9065d404975b70442916c35635c4
MiguelGH01 <144173122+MiguelGH01@users.noreply.github.com>
2024-04-18 18:14:25 +0200

    Transformación PGIS a df_combinado

---
+++ codigo-migui/integracion-data-radiacion.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+import os
+
+# Especificar la ruta de la carpeta
+ruta_carpeta = "./csv_eur"
+
+# Lista para almacenar los DataFrames
+dataframes = []
+
+# Recorrer la carpeta y leer cada archivo CSV
+for archivo in os.listdir(ruta_carpeta):
+    if archivo.endswith(".csv"):
+        
+        # Crear un DataFrame para el archivo CSV actual
+        df = pd.read_csv(f"{ruta_carpeta}/{archivo}")
+        
+        df[["year", "month", "H(h)_m"]] = df["year\t\tmonth\t\tH(h)_m"].str.split("\\t\\t", expand=True)
+        del df["year\t\tmonth\t\tH(h)_m"]
+        
+        # Asignar un nombre descriptivo al DataFrame (opcional)
+        df.name = archivo[:-4]  # Eliminar la extensión .csv
+
+        # Agregar el DataFrame a la lista
+        dataframes.append(df)
+
+df_combinado = pd.DataFrame()  # DataFrame vacío para almacenar el resultado final
+
+month_num_map = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6,
+                  "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}
+
+# Recorrer y combinar DataFrames
+for df in dataframes:
+    # Asegurar orden consistente de "year" y "month"
+    if df["year"].name != "year" or df["month"].name != "month":
+        df = df.rename(columns={"year": "year", "month": "month"})
+
+    # Unificar columnas "year" y "month" con el DataFrame actual
+    if df_combinado.empty:
+        df_comun = df[["year", "month"]]
+    else:
+        df_comun = pd.concat([df_combinado, df[["year", "month"]]], ignore_index=True)
+
+    # Agregar columna "H(h)_m" con el nombre del DataFrame actual
+    df_comun[f"{df.name}"] = df["H(h)_m"]
+    df_combinado = df_comun
+    
+# Diccionario para mapear nombres de meses a números
+# month_num_map = {"Jan": 1, "Feb": 2, "Mar": 3, "Apr": 4, "May": 5, "Jun": 6,
+#                   "Jul": 7, "Aug": 8, "Sep": 9, "Oct": 10, "Nov": 11, "Dec": 12}
+
+
+# df_combinado["month"] = df_combinado["month"].map(month_num_map)
+
+# # Crear columna "Fecha"
+# df_combinado["Fecha"] = df_combinado.apply(lambda row: f"{row['year']}", axis=1)
+# df_combinado["Fecha"] = df_combinado.apply(lambda row: f"{row['year']}_{str(row['month']).zfill(2)}", axis=1)
+
+# # Mover "Fecha" a la primera posición
+# df_combinado = df_combinado[['Fecha'] + list(df_comun.columns[2:-1])]
+
+# # Ordenar por Fecha
+# df_combinado = df_combinado.sort_values(by=['Fecha'])
+ 
+# Procesar o analizar los DataFrames individuales
+print(df_combinado.head())  # Mostrar las primeras filas del DataFrame
