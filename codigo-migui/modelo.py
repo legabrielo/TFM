537c9fe6d0dc0558f142f5db9622ba86c7ccdd57
MiguelGH01 <miguelgh2001@gmail.com>
2024-05-23 17:49:53 +0200

    Retoque a Recomendación y hecho la parte de la gráfica

--- codigo-migui/modelo.py
+++ codigo-migui/modelo.py
@@ -69,6 +69,8 @@ def analisis_lugar(lugar):
     distancias = distancias.sort_values(by='Distancia')
     return distancias.head(50)
 
+modos = ['Distancia', 'Radiación']
+
 def recomendacion_pasada(mejores_lugares, modo):
     '''
     Esta función calcula la puntuación de los mejores lugares donde se podría haber instalado una planta fotovoltáica.
@@ -94,7 +96,22 @@ def recomendacion_pasada(mejores_lugares, modo):
             mejores_lugares.at[index, 'Puntuacion'] = 0
     
     mejores_lugares.drop(['E_norm', 'D_norm'], axis=1, inplace=True)
-    return mejores_lugares.head(10)
+    return mejores_lugares.sort_values('Puntuacion', ascending=False).head(10)
+
+def recomendacion_streamlit(mejores_lugares):
+    mejores_lugares_st = mejores_lugares.copy()
+    mejores_lugares_st.index = range(1, 11)
+    mejores_lugares_st['Puntuacion'] = round(mejores_lugares_st['Puntuacion'] * 100,2)
+    def add_percent(value):
+        return f"{value:.2f} %"
+    mejores_lugares_st['Puntuacion'] = mejores_lugares_st['Puntuacion'].apply(add_percent)
+    mejores_lugares_st.rename(columns={'Subestacion': 'Subestación',
+                                    'Provincia': 'Provincia',
+                                    'Distancia': 'Distancia',
+                                    'kWh_euro_de_terreno': 'kWh por euro de terreno',
+                                    'Puntuacion': 'Puntuacion'}, inplace=True)
+    
+    return mejores_lugares_st
 
 def radiacion_pasada(mejores_lugares, lugar):
     '''
@@ -104,35 +121,38 @@ def radiacion_pasada(mejores_lugares, lugar):
         mejores_lugares (DataFrame): DataFrame con la infromación para realizar la recomendación.
         lugar (str): Ubicación para la gráfica.
     '''
+        
     mejores_lugares = mejores_lugares[mejores_lugares['Subestacion'] == lugar]
     provincia = mejores_lugares['Provincia'].values[0]
+    def get_color(lugar):
+        colors = ['#1E90FF', '#32CD32', '#FF69B4', '#FFD700', '#FF4500', '#7B68EE', '#00CED1', '#FF1493', '#8A2BE2', '#00FA9A']
+        index = mejores_lugares[mejores_lugares['Subestacion'] == lugar].index[0]
+        color_index = index % len(colors)
+        return colors[color_index]
+        
     radiacion = df_radiacion[provincia]
     fechas = pd.date_range(start='2010-01-01', end='2023-12-01', freq='MS')
     df = pd.DataFrame({'Radacion': radiacion.values}, index=fechas)
+    df.rename(columns={'Radacion': 'Radiación (kWh)'}, inplace=True)
+    df.index.name = 'Fecha'
     
-    df.plot(kind='line', color='orange')
-    plt.ylabel('Radiación')
-    plt.title(f'Radiacion Pasada en {lugar}')
-    plt.ylim(0)
-    plt.grid(axis='y', linestyle='dashed')
-    plt.show()
-    return df
+    return st.area_chart(df, color=get_color(lugar), use_container_width=True)
 
-st.title("Análisis de Ubicaciones para Plantas Fotovoltaicas")
+
+st.title("Análisis de ubicaciones para plantas fotovoltaicas")
 
 ubicacion = st.text_input("Indique la localidad que desea analizar:")
+modo = st.selectbox("Seleccione el modo de recomendación:", modos)
 
-if st.button("Analizar"):
-    geolocator = geocoders.Nominatim(user_agent="tu_aplicacion")
-    lugar = geolocator.geocode(ubicacion)
+geolocator = geocoders.Nominatim(user_agent="tu_aplicacion")
+lugar = geolocator.geocode(ubicacion)
+
+if lugar is not None:
+    analisis = analisis_lugar(lugar)
+    recomendacion = recomendacion_pasada(analisis, modo)
+    st.dataframe(recomendacion_streamlit(recomendacion), use_container_width=True)
     
-    if lugar is not None:
-        analisis = analisis_lugar(lugar)
-        recomendacion = recomendacion_pasada(analisis, 'Radiación')
-        st.write(recomendacion.sort_values('Puntuacion', ascending=False))
+    lugar_seleccionado = st.selectbox("Seleccione una subestación:", recomendacion['Subestacion'].values)
+    radiacion_pasada(recomendacion, lugar_seleccionado)
+    
         
-        lugar_seleccionado = st.selectbox("Seleccione una subestación:", recomendacion['Subestacion'].values)
-        radiacion_pasada(recomendacion, lugar_seleccionado)
-    else:
-        st.write("No se pudo encontrar la ubicación. Por favor, intente nuevamente.")
-            
