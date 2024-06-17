85f93e97cc23a7c36474eb1216c703de49ab5848
MiguelEY01 <miguelgh2001@gmail.com>
2024-06-17 14:09:21 +0200

    Auto-arima, jusficaciones

--- codigo-migui/prediccion.py
+++ codigo-migui/prediccion.py
@@ -1,8 +1,6 @@
 import pandas as pd
-import numpy as np
 from pmdarima import auto_arima
-from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.tsa.stattools import acf, pacf, adfuller, kpss
+from statsmodels.tsa.stattools import adfuller
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 import matplotlib.pyplot as plt
 import warnings
@@ -17,24 +15,6 @@ for column in df.columns[1:]:
     df_provincia = df[['Fecha', column]]
     df_provincia.name = column
     dfs.append(df_provincia)
-
-# Crear un modelo Auto-ARIMA para cada provincia
-predictions = []   
-def modelo_auto_ARIMA(dfs):
-    for df_provincia in dfs:
-        model = auto_arima(df_provincia.iloc[:, 1], seasonal=False, suppress_warnings=True)
-        p = model.order[0]
-        d = model.order[1]
-        m = model.order[2]
-        print(f'{df_provincia.name} \n  p: {p} d: {d} m: {m}')
-        # model_fit = model.fit(y=df_provincia.iloc[:, 1])  # Pass 'y' explicitly
-
-        # # Make predictions
-        # provincia_predictions = model_fit.predict(n_periods=36)
-        # predictions.append(provincia_predictions)
-        # print(f'Predicciones para {df_provincia.name}:\n{provincia_predictions}')
-        # break
-    # Esto es una prueba, habría que ver que eficacia tiene el modelo...
 
 def grafica_acf(provincia):
     for df_provincia in dfs:
@@ -71,7 +51,7 @@ def grafica_pacf(provincia):
             
             plt.show()
             break
-        
+
 def grafica_acf_pacf(provincia):
     for df_provincia in dfs:
         if df_provincia.name == provincia:
@@ -90,66 +70,72 @@ def grafica_acf_pacf(provincia):
             plt.show()
             break
 
-def comprobar_estacionariedad(df):
+def ad_fuller(df):
     result = adfuller(df)
-    return result[1]  # p-valor
+    print('ADF Statistic: %f' % result[0])
+    print('p-value: %f' % result[1])
 
-def determinar_d(df, max_diff=2, significance_level=0.05):
-    """
-    Determina el número de diferenciaciones necesarias para estacionarizar la serie.
-    :param series: Serie temporal (pandas Series).
-    :param max_diff: Número máximo de diferenciaciones a probar.
-    :param significance_level: Nivel de significancia para la prueba de ADF.
-    :return: Mejor valor de d.
-    """
-    d = 0
-    p_value = comprobar_estacionariedad(df)
+def predicciones_auto_ARIMA(dfs, año):
+    # Creamos una serie de tiempo con fechas desde 2021-01 hasta 2023-12, esto cambiara de 2024-01 a 2026-12
+    predictions = []
+    meses = []
+    num_pred = (año-2023)*12
+    for año in range(2010, año):
+        # Iterate through months (1 to 12)
+        for mes in range(1, 13):
+            # Create the month string in the desired format
+            mes_str = f"{año}_{mes:02d}"
+            meses.append(mes_str)
+            
+    tiempo = pd.Series(meses)
     
-    while p_value > significance_level and d < max_diff:
-        d += 1
-        df = df.diff().dropna()
-        p_value = comprobar_estacionariedad(df)
-    
-    return d
-
-def determinar_p(df, max_diff=2, significance_level=0.05):
-    pacf_values, conf_interval = pacf(df, nlags=max_diff, alpha=significance_level)
-    i=0
-    p=0
-    signo_inicial="+" if pacf_values[0]>=0 else "-"
-    for value in pacf_values:
-        signo_actual="+" if pacf_values[i]>=0 else "-"
-        if abs(value) < conf_interval[i][1] - value:
-            p = i+1
-            break
-        elif signo_inicial != signo_actual:
-            p = i+1
-            break
-        else:
-            p=0
-        i=i+1
-    return p
-
-def determinar_m(df, max_diff=2, significance_level=0.05):  
-    return 0      
-# Creamos un modelo ARIMA para cada provincia
-def modelo_ARIMA(dfs):
-    for df_provincia in dfs: 
-        # Diferenciación   
-        d = determinar_d(df_provincia.iloc[:, 1])
+    for df_provincia in dfs:
+        model = auto_arima(df_provincia.iloc[:, 1], m=12, seasonal=True, suppress_warnings=True)
+        model_fit = model.fit(y=df_provincia.iloc[:, 1])  # Pass 'y' explicitly
+        print(model_fit.summary())
         
-        # Autorregresión
-        p = determinar_p(df_provincia.iloc[:, 1])
-
-        # Medias móviles
-        m = determinar_m(df_provincia.iloc[:, 1])
+        # Make predictions
+        provincia_predictions = model_fit.predict(n_periods=num_pred)
+        predictions.append(provincia_predictions)
+        print(f'Predicciones para {df_provincia.name}:\n{provincia_predictions}')
         
-        print(f'{df_provincia.name} \n p: {p} d: {d} m: {m}')
+        # Falta volcarlo el otro excel, juntarlo...
+        # Nos centramos en justificar el modelo, en redactar vaya
+        break
 
+def resumen_modelo(provincia):
+    for df_provincia in dfs:
+        if df_provincia.name == provincia:
+            model = auto_arima(df_provincia.iloc[:, 1], seasonal=True, suppress_warnings=True)
+            print(model.summary())
 
-# grafica_acf_pacf("Las Palmas")
-# modelo_ARIMA(dfs)
-# modelo_auto_ARIMA(dfs)
+def prediccion(provincia, año):
+    num_pred = (año-2023)*12
+    for df_provincia in dfs:
+        if df_provincia.name == provincia:
+            train,test=df_provincia[:-24],df_provincia[-24:]
+            model = auto_arima(train.iloc[:, 1], trace=True, m=12, seasonal=True, suppress_warnings=True)
+            model = model.fit(y=train.iloc[:, 1])  # Pass 'y' explicitly
+            provincia_predictions = model.predict(n_periods=24)   
+            plt.plot(train.iloc[:, 1], label='Train')
+            plt.plot(test.iloc[:, 1], label='Valid')   
+            plt.plot(provincia_predictions, label='Prediction')
+            plt.legend()
+            plt.show()
+
+def residuos(provincia):
+    for df_provincia in dfs:
+        if df_provincia.name == provincia:
+            model = auto_arima(df_provincia.iloc[:, 1], seasonal=True, suppress_warnings=True)
+            residuos = model.resid()
+            plt.plot(residuos)
+            plt.title('Residuos')
+            plt.show()
+
+# predicciones_auto_ARIMA(dfs, 2030)
+# resumen_modelo('Almeria')
+# residuos('Malaga')
+prediccion('Lerida',2030)
 
 
 
