76472f1bb12f92f9d161021832e2af7c70a35ec8
Gabriel Castillo <92647404+legabrielo@users.noreply.github.com>
2024-06-19 19:43:30 +0200

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -6,49 +6,51 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os 
-# from sklearn.preprocessing import OneHotEncoder
-# from sklearn.neighbors import NearestNeighbors
+from sklearn.cluster import KMeans
 import pickle 
 
 
-features = pd.read_csv(r'features.csv')
+model_path = r'C:\Users\Gabriel Castillo\Desktop\Cosas Gabriel\Máster\kmeans_model.pkl'
 res = pd.read_csv(r'total_df.csv')
 
-modelo = pickle.load(open('Modelo_Radiacion.pkl', 'rb'))
+def load_model(path):
+    with open(path, 'rb') as file:
+        return pickle.load(file)
 
-def dar_prediccion(filtered_res):
-            liked_PALCE = filtered_res.iloc[0]
-            print(liked_PALCE)
-            num_recom = 8
-            _, ind = modelo.kneighbors(features.iloc[[liked_PALCE.name]])
-        
-            st.subheader('Recommended Restaurants')
-            rec_indices = ind[0][0:num_recom]
-            rec_resta = res.iloc[rec_indices, :]
-            st.write(rec_resta)
+model = load_model(model_path)
 
+def dar_prediccion(selected_provincia, selected_value):
+    filtered_df = res[res['Provincia'] == selected_provincia]
+    filtered_df['Adjusted Value'] = filtered_df['rad-media'] * selected_value
+    sorted_df = filtered_df.sort_values(by='Adjusted Value', ascending=False)
     
+    return sorted_df
+
 
 def main():
-    st.title('Recomendador de suelos para construcción de parque fotovolataícos')
+    st.title('Recomendador de suelos para construcción de parque fotovoltaicos')
 
-    selected_provincia = st.selectbox("Seleccione Provincia",res['Provincia'].unique().astype(str))
-    selected_rad = st.selectbox("Seleccione Radiacion",res['rad-media'].unique().astype(str))
-
-    filtered_res = res[
-        (res['Provincia'] == selected_provincia) &
-        (res['rad-media'] == selected_rad) 
-    ]
-    print('This is filtered res:' '\n', filtered_res)
-    # Añadir aquí un buclque para que primero pono ponga nada (done)
-
-    if filtered_res.empty:
-        if st.button("Obtener Terrenos"):
-            st.write('No hay recomendación todavía')
-    else:
-        if st.button("Obtener Terrenos"):
-            dar_prediccion(filtered_res)
+    selected_provincia = st.selectbox("Seleccione Provincia", res['Provincia'].unique().astype(str))
+    selected_value = st.selectbox("Seleccione un valor", [50000, 1000000, 150000, 200000])
 
 
-if __name__ =="__main__":
+    if st.button("Obtener predicción"):
+        sorted_df = dar_prediccion(selected_provincia, selected_value)
+        st.write(f"Predicción para {selected_provincia} con valor {selected_value}")
+        st.dataframe(sorted_df)
+
+if __name__ == "__main__":
     main()
+
+
+
+
+        # print('This is filtered res:' '\n', filtered_res)
+    # # Añadir aquí un buclque para que primero pono ponga nada (done)
+
+    # if filtered_res.empty:
+    #     if st.button("Obtener Terrenos"):
+    #         st.write('No hay recomendación todavía')
+    # else:
+    #     if st.button("Obtener Terrenos"):
+    #         dar_prediccion(filtered_res)
